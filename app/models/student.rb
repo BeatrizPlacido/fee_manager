76c8aec7abb8bfa651e2042429e56a911f41c8ee
BeatrizPlacido <biaplacsilva0212@gmail.com>
2024-07-19 18:05:06 -0300

    add cpf validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,12 +10,34 @@ class Student < ApplicationRecord
   private
 
   def cpf_is_valid?
-    
+    first_verifying_digit(format_cpf) == format_cpf[-2] && second_verifying_digit(format_cpf) == format_cpf[-1]
   end
 
   def format_cpf
-    array.gsub('.', '').gsub('-', '').chars
+    @format_cpf ||= cpf.gsub('.', '').gsub('-', '').chars.map{ |char| char.to_i }
   end
 
+  def first_verifying_digit(format_cpf)
+    aux = 10
+    result = 0
 
+    format_cpf.slice(0, 9).each do |digit|
+      result += digit * aux
+      aux -= 1
+    end
+
+    (result % 11) < 2 ? 0 : 11 - (result % 11)
+  end
+
+  def second_verifying_digit(format_cpf)
+    aux = 11
+    result = 0
+
+    format_cpf.slice(0, 10).each do |digit|
+      result += digit * aux
+      aux -= 1
+    end
+
+    (result % 11) < 2 ? 0 : 11 - (result % 11)
+  end
 end
